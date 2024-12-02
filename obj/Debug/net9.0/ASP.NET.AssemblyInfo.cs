df18950fc7067efc7a0f5893d4740dff84efacdb
CanberkAkar <akarcanberk@gmail.com>
2024-12-02 21:47:01 +0300

    created global import file

--- obj/Debug/net9.0/ASP.NET.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8613d54117c8f9e718f9f7c01e9a86a0d7001d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b03262c87273924719948982995b4a304ab2382")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
