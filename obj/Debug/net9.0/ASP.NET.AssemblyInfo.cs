1626dd2b8daf1890b69e73e05250c7c2e1cfc9e8
CanberkAkar <akarcanberk@gmail.com>
2024-12-05 17:26:25 +0300

    bug fixed

--- obj/Debug/net9.0/ASP.NET.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASP.NET.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f470fb5418797037979c4eb632a87b83ad3df90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71367e6c715f83a4ad8e15f5d6756445bc4fb9cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
